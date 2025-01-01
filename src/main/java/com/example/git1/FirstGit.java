eba756d14c18561a73361b205a5de9b2da244538
euphoriaHC <alexlatkin3@gmail.com>
2025-01-01 16:58:32 +0300

    add first sout in FirstGit class

--- src/main/java/com/example/git1/FirstGit.java
+++ src/main/java/com/example/git1/FirstGit.java
@@ -3,7 +3,7 @@ package com.example.git1;
 public class FirstGit {
     public static void main(String[] args) {
 
-        System.out.println("git");
+        System.out.println("git develop");
         System.out.println("git2");
 
     }
